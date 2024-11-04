474e26d0fe2b435a35b7729bd72a5740a477a340
Janvi Bajaj <janvi.bajaj@digiqt.com>
2024-11-04 21:04:22 +0530

    divider added

--- src/components/Response2.jsx
+++ src/components/Response2.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Box, Typography, Tabs, Tab, CircularProgress } from "@mui/material";
+import { Divider } from "@mui/material";
+
 import Header from "./Header";
 import Footer from "./Footer";
 import axios from "axios";
@@ -9,6 +11,219 @@ const ResponseRender = () => {
   const [loading, setLoading] = useState(true);
 
   const [data, setData] = useState(null);
+
+  //   const [data, setData] = useState([
+  //     {
+  //       title: "Income",
+  //       data: [
+  //         {
+  //           description: "IMPS-319507865074-RESILIENT INNOVATION",
+  //           amount: 50769.94,
+  //           timestamp: "2024-06-15T07:23:35+05:30",
+  //         },
+  //         {
+  //           description: "UPI-BHARATPE PAYOUTS-CASHFREEBHARATP@YESBANK",
+  //           amount: 38602,
+  //           timestamp: "2024-06-16T20:53:39+05:30",
+  //         },
+  //         {
+  //           description: "UPI-KATHIRVEL R-9952144145@YBL",
+  //           amount: 1300,
+  //           timestamp: "2024-06-24T15:40:20+05:30",
+  //         },
+  //         {
+  //           description: "IMPS-223212545821-BHARATPE-ICIC",
+  //           amount: 23000,
+  //           timestamp: "2024-07-23T17:56:52+05:30",
+  //         },
+  //         {
+  //           description: "UPI-G DEEPAN-DEEPANGOVINDARAJ11@OKICICI",
+  //           amount: 26240,
+  //           timestamp: "2024-07-09T11:12:41+05:30",
+  //         },
+  //         {
+  //           description: "MILLENNIA CARD CASH BACK",
+  //           amount: 400,
+  //           timestamp: "2024-07-06T18:08:40+05:30",
+  //         },
+  //         {
+  //           description: "UPI-BHARATPE PAYOUTS-CASHFREEBHARATP@YESBANK",
+  //           amount: 20000,
+  //           timestamp: "2024-08-03T08:55:42+05:30",
+  //         },
+  //         {
+  //           description: "UPI-VIDHYASHANKAR M-7401393801@AXL",
+  //           amount: 10502,
+  //           timestamp: "2024-09-15T23:28:25+05:30",
+  //         },
+  //         {
+  //           description: "UPI-PAYTM-PAYOUTS@PAYTM-PYTM0123456",
+  //           amount: 44833.95,
+  //           timestamp: "2024-08-22T00:50:45+05:30",
+  //         },
+  //         {
+  //           description: "UPI-PAYTM-PAYOUTS@PAYTM-PYTM0123456",
+  //           amount: 49996,
+  //           timestamp: "2024-10-02T17:53:07+05:30",
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       title: "Expenses",
+  //       data: [
+  //         {
+  //           category: "Utilities",
+  //           transactions: [
+  //             {
+  //               description: "UPI-LAZYPAY PRIVATE LIMI-LAZYPAYPVTLTD",
+  //               amount: 190,
+  //               timestamp: "2024-08-07T07:37:00+05:30",
+  //             },
+  //             {
+  //               description: "UPI-ADD MONEY TO WALLET-ADD-MONEY@PAYTM",
+  //               amount: 259.68,
+  //               timestamp: "2024-09-15T13:05:10+05:30",
+  //             },
+  //             {
+  //               description: "UPI-MADHUR CHAI-Q630729614@YBL",
+  //               amount: 60,
+  //               timestamp: "2024-09-29T15:01:52+05:30",
+  //             },
+  //             {
+  //               description: "UPI-AMUL PARLOUR-Q855461190",
+  //               amount: 190,
+  //               timestamp: "2024-09-09T16:12:00+05:30",
+  //             },
+  //           ],
+  //         },
+  //         {
+  //           category: "Grocery",
+  //           transactions: [
+  //             {
+  //               description: "UPI-DHANAPAL FANCY STORE-PAYTM",
+  //               amount: 170,
+  //               timestamp: "2024-08-04T17:11:42+05:30",
+  //             },
+  //             {
+  //               description: "UPI-BIGBASKET-BBNOW@YBL",
+  //               amount: 500,
+  //               timestamp: "2024-10-05T16:20:55+05:30",
+  //             },
+  //           ],
+  //         },
+  //         {
+  //           category: "Healthcare",
+  //           transactions: [
+  //             {
+  //               description: "UPI-STAR PHARMACY-BHARATPE09897973834",
+  //               amount: 80,
+  //               timestamp: "2024-07-15T11:55:48+05:30",
+  //             },
+  //             {
+  //               description: "UPI-BCS PHARMACY-BCSPHARMACY",
+  //               amount: 56,
+  //               timestamp: "2024-10-04T07:09:09+05:30",
+  //             },
+  //           ],
+  //         },
+  //         {
+  //           category: "Entertainment",
+  //           transactions: [
+  //             {
+  //               description: "MILLENNIA CARD CASH BACK",
+  //               amount: 400,
+  //               timestamp: "2024-07-06T18:08:40+05:30",
+  //             },
+  //             {
+  //               description: "UPI-GRAND CINEMAS-PAYTM",
+  //               amount: 160,
+  //               timestamp: "2024-09-22T17:19:49+05:30",
+  //             },
+  //             {
+  //               description: "UPI-INNOVATIVE MULTIPLEX-PAYENT",
+  //               amount: 359,
+  //               timestamp: "2024-07-05T14:25:14+05:30",
+  //             },
+  //           ],
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       title: "Debts",
+  //       data: [
+  //         {
+  //           description: "FD THROUGH NET",
+  //           amount: 100000,
+  //           timestamp: "2024-10-02T13:10:00+05:30",
+  //         },
+  //         {
+  //           description: "UPI-KONDAMUDI SURI PRAKA-JONAVIJAY1",
+  //           amount: 13000,
+  //           timestamp: "2024-09-05T20:29:15+05:30",
+  //         },
+  //         {
+  //           description: "UPI-MD AIZHAR-MDEBRARALAMMDEBRARALAM",
+  //           amount: 5300,
+  //           timestamp: "2024-09-09T20:30:22+05:30",
+  //         },
+  //         {
+  //           description: "UPI-VENUGOPAL M",
+  //           amount: 1600,
+  //           timestamp: "2024-09-15T20:54:41+05:30",
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       title: "Assets and Investments",
+  //       data: [
+  //         {
+  //           description: "FD THROUGH NET",
+  //           amount: 100000,
+  //           timestamp: "2024-10-02T13:10:00+05:30",
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       title: "Insurance Policies",
+  //       data: [
+  //         {
+  //           description: "TATAAIAPOLICY_U204046624",
+  //           amount: 4998,
+  //           timestamp: "2024-07-25T16:11:39+05:30",
+  //         },
+  //         {
+  //           description: "ACH D- POLICYBAZAAR",
+  //           amount: 2460,
+  //           timestamp: "2024-06-29T15:27:57+05:30",
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       title: "Financial Health Assessment",
+  //       data: {
+  //         "Debt-to-Income Ratio": 0.42,
+  //         "Disposable Income": 120000,
+  //       },
+  //     },
+  //     {
+  //       title: "Insurance Recommendations",
+  //       data: [
+  //         {
+  //           "Recommended Product": "Credit Health Premium",
+  //           "Sum Insured": 500000,
+  //           Premium: 5000,
+  //           Reason: "Debt-to-Income Ratio exceeds threshold",
+  //         },
+  //         {
+  //           "Recommended Product": "Health Value",
+  //           "Sum Insured": 300000,
+  //           Premium: 3000,
+  //           "Number of Insured Members": 1,
+  //           Reason: "Moderate healthcare spending with a basic coverage need",
+  //         },
+  //       ],
+  //     },
+  //   ]);
 
   const handleTabChange = (event, newValue) => {
     setTabIndex(newValue);
@@ -50,7 +265,11 @@ const ResponseRender = () => {
     if (Array.isArray(dataItem)) {
       return dataItem.map((item, index) => (
         <Box key={index} sx={{ marginBottom: 2 }}>
-          {renderData(item)} {/* Recursive call for array items */}
+          {renderData(item)}
+          {index < dataItem.length - 1 && (
+            <Divider sx={{ backgroundColor: "black" }} />
+          )}{" "}
+          {/* Add divider between items */}
         </Box>
       ));
     } else if (typeof dataItem === "object" && dataItem !== null) {
@@ -62,9 +281,7 @@ const ResponseRender = () => {
           <Typography variant="h6" sx={{ fontWeight: "bold", marginRight: 1 }}>
             {key.charAt(0).toUpperCase() + key.slice(1)}:
           </Typography>
-          <Box sx={{ marginLeft: 1 }}>
-            {renderData(value)} {/* Recursive call for object values */}
-          </Box>
+          <Box sx={{ marginLeft: 1 }}>{renderData(value)}</Box>
         </Box>
       ));
     } else {
@@ -95,7 +312,6 @@ const ResponseRender = () => {
             overflowY: "auto",
             padding: 2,
             margin: "0 auto",
-            maxWidth: "1200px", // Set a max width for widespread appearance
             backgroundColor: "#fff",
             borderRadius: 2,
             boxShadow: "0px 4px 12px rgba(36, 180, 179, 0.8)",
@@ -147,6 +363,7 @@ const ResponseRender = () => {
                           <Box className="description">
                             {renderData(item.data)} {/* Call to renderData */}
                           </Box>
+                          <Divider />
                         </>
                       )}
                     </Box>
